53a20a492e6d764afbc73da90ca01d5f93cd56d5
pokiii <winkoo8788@gmail.com>
2026-09-10 08:44:47 +0700

    Add files via upload
    Ghalang

---
+++ arrayassco.php
@@ -0,0 +1,16 @@
+<?php
+
+$dataBarang = [
+    "nama_data_barang_1" => "proyektor",
+    "nama_data_barang_2" => "jendela",
+    "nama_data_barang_3" => "lampu",
+
+];
+
+echo "data nama barang 1 " , $dataBarang["nama_data_barang_1"];
+echo "<br/>";
+echo "data nama barang 2 " , $dataBarang["nama_data_barang_2"];
+echo "<br/>";
+echo "data nama barang 3 " , $dataBarang["nama_data_barang_3"];
+
+?><?php
